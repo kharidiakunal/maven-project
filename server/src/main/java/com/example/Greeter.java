e01bc2bb45dfe41c4fc168a969cfcbc10be3de65
Kunal Kharidia <kunalkharidia@gmail.com>
2018-12-15 19:39:12 -0500

    Checkstyle changes again

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,9 @@ public class Greeter {
   }
   
   /**
-  * @param the name of person
+  * @param someone the name of person
   * @return greeting string
   */
-
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
